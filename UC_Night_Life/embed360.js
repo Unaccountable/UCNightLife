9d31fa3d72a3cdd045cb51fcaee7b71a368375d7
Billy <Unaccountable@users.noreply.github.com>
2021-10-17 18:20:49 -0400

    initial website commit
    homepage partially done
    some photosphere images

---
+++ UC_Night_Life/embed360.js
@@ -0,0 +1,41 @@
+function embed360(selectors, aframeSrc) {
+  aframeSrc = aframeSrc || "https://aframe.io/releases/0.8.2/aframe.min.js";
+  //aframeSrc = aframeSrc || "https://aframe.io/releases/1.2.0/aframe.min.js";
+  var elementList = document.querySelectorAll(selectors);
+  for (var i = 0; i < elementList.length; ++i) {
+    var el = elementList[i];
+    var img = el.querySelector('img');
+    if (img) {
+      makeIframe(el, img);
+    }
+  }
+
+  function makeIframe(el, img) {
+    var imgsrc = img.src;
+    el.removeChild(img);
+    var rotation = el.dataset.rotation || "";
+    var iframe = document.createElement('iframe');
+    var content = '<!DOCTYPE html>' +
+      '<head><title>A photo in aframe in an iframe</title>' +
+      '<script type="text/javascript" src="https://cdn.rawgit.com/leemark/777b5ce94bce409b5f208c924bb5878b/raw/4958a8a59a50f18b5f8cbaa00c9209082dd2dfdf/customeventpolyfill.js"><\/script>' +    
+      '<script type="text/javascript" src="' + aframeSrc + '"><\/script><\/head>' +
+      '<body>' +
+      '<a-scene>' +
+      '<a-assets>' +
+      '<img id="sky" src="' + imgsrc + '">' +
+      '</a-assets>' +
+      '<a-sky src="#sky" rotation="' + rotation + '"></a-sky>' +
+      '</a-scene>' +
+      '</body></html>';
+    iframe.setAttribute('allowfullscreen', 'yes');
+    iframe.setAttribute('allowvr', 'yes');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('aria-hidden', 'true');
+    el.appendChild(iframe);
+    iframe.contentWindow.document.open('text/html', 'replace');
+    iframe.contentWindow.document.write(content);
+    iframe.contentWindow.document.close();
+  }
+}
